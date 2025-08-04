178c2ebd47405ddbc15979448c1d94e3ae6cba0b
Priyadp02 <devipriya9332@mountzion.ac.in>
2025-08-04 12:26:36 +0530

    Create statement.java

---
+++ statement.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+       int a=20;
+       int b=19;
+    if (a<b){
+         System.out.println("a is younger than b");
+    }
+    else{
+        System.out.println("b is elder than a");
+    }
+}
+}
