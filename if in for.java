4ad6e6b049998304a3bfde5e5319751a52377bf5
Priyadp02 <devipriya9332@mountzion.ac.in>
2025-08-05 11:52:54 +0530

    Create if in for.java

---
+++ if in for.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        
+        for(int count=1;count<=10;count++){
+            if (count==5){
+                break;
+            }
+        
+        
+            System.out.println(count);
+        }
+        
+    } 
+}
