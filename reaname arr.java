395a61dfb8b2cd1e3c67ec8b5f65610126a0bd2c
Priyadp02 <devipriya9332@mountzion.ac.in>
2025-08-06 12:25:38 +0530

    reaname arr.java

--- reaname arr.java
+++ reaname arr.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] names = new String[5];
-        System.out.println("Enter 5 names:");
+        System.out.println("Enter 3 names:");
         for (int i = 0; i < 5; i++) names[i] = sc.nextLine();
         System.out.print("Name to delete: ");
         String del = sc.nextLine();
