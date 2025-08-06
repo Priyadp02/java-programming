3e75742bef7e28c6c2ab516a9a21956ccf4b0dd8
Priyadp02 <devipriya9332@mountzion.ac.in>
2025-08-06 09:31:14 +0530

    Create arr.java

---
+++ arr.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        String[] array={"devi","pravee",
+    "asmi","lali","arch","maha"};
+        array[1]="arch";
+        System.out.println(array[1]);
+    }
+}
+
