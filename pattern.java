e9e9b7298894312862a1af6824c75d7d89dca7f6
Priyadp02 <devipriya9332@mountzion.ac.in>
2025-08-07 12:34:10 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -27,3 +27,76 @@ public class Main {
 }
 
 
+// rhombus
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 3;
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print("  ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+           if (i == n) {
+                for (int s = 1; s <= n - i; s++) {
+                    System.out.print("  ");
+                }
+                for (int j = i; j >= 1; j--) {
+                    System.out.print(j + " ");
+                }
+                for (int j = 2; j <= i; j++) {
+                    System.out.print(j + " ");
+                }
+                System.out.println();
+            }
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print("  ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//doublepyramid
+public class DoublePyramid {
+    public static void main(String[] args) {
+        int n=6; 
+        for (int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=n-i;j++) 
+            {
+                System.out.print(" ");
+            }
+            for (int k=1;k<=i;k++) 
+            {
+                System.out.print("- ");
+            }
+            for (int j=1;j<=(n-i)*2;j++) 
+            {
+                System.out.print(" ");
+            }
+            for (int k=1;k<=i;k++) 
+            {
+                System.out.print(" +");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
