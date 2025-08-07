e0340a299649b5449c2a2d9c9b9635339b464317
Priyadp02 <devipriya9332@mountzion.ac.in>
2025-08-07 09:43:46 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -1,3 +1,4 @@
+//pattern
 public class Main {
     public static void main(String[] args) {
         int n = 7;
@@ -10,3 +11,19 @@ public class Main {
         }
     }
 }
+
+// reverse order
+public class Main {
+    public static void main(String[] args) {
+        int n = 7;
+
+        for (int i = n; i >= 1; i--) { 
+            for (int j = 1; j <= i; j++) { 
+                System.out.print(j + " ");
+            }
+            System.out.println(); 
+        }
+    }
+}
+
+
