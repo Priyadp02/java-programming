56701f9bc52297a009462200fae9bb94ab7d03aa
Priyadp02 <devipriya9332@mountzion.ac.in>
2025-08-08 10:27:29 +0530

    Update methods.java

--- methods.java
+++ methods.java
@@ -49,5 +49,26 @@ public class Main {
     }
 }
 
-
+//biodata
+import java.util.Scanner;
+public class Main {
+    static void myMethod(String firstName, String lastName, int age, double height) {
+        System.out.println("Full Name: " + firstName + " " + lastName);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+    }
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        System.out.print("Enter the first name: ");
+        String first = data.next();
+        System.out.print("Enter the last name: ");
+        String last = data.next();
+        System.out.print("Enter the Age: ");
+        int age = data.nextInt();
+        System.out.print("Enter the height: ");
+        double height = data.nextDouble();
+        myMethod(first, last, age, height);
+        data.close();
+    }
+}
 
