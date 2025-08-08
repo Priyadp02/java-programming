8f64d6d43ac843bbbe06bce97a477a07c303ab93
Priyadp02 <devipriya9332@mountzion.ac.in>
2025-08-08 10:17:46 +0530

    Create methods.java

---
+++ methods.java
@@ -0,0 +1,53 @@
+// basic
+public class Main {
+    public static void main(String[] args) {
+        myMethod(); 
+    }
+
+    public static void myMethod() {
+        System.out.println("DEVI M");
+    }
+}
+
+
+// integers 
+import java.util.Scanner;
+
+public class Main {
+    static void mymethod(int x, int y) {
+        System.out.println(x + y);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        mymethod(a, b); 
+    }
+}
+
+
+//name
+import java.util.Scanner;
+public class Main {
+    static void myMethod(String firstName, String lastName) {
+        System.out.println("Full Name: " + firstName + " " + lastName);
+    }
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        System.out.print("Enter first name: ");
+        String first = data.next();
+        System.out.print("Enter last name: ");
+        String last = data.next();
+        myMethod(first, last);
+        data.close();
+    }
+}
+
+
+
